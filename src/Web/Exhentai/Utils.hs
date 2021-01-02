6a092718062c3591bd5029a40fc43b49942a50f5
Poscat <poscat@mail.poscat.moe>
2021-01-03 02:31:00 +0800

    Add some helper Traversals for parsing HTML

--- src/Web/Exhentai/Utils.hs
+++ src/Web/Exhentai/Utils.hs
@@ -13,3 +13,36 @@ attributeSatisfies' n p = filtered (p . preview (attrs . ix n))
 
 withoutAttribute :: Name -> Traversal' Element Element
 withoutAttribute = flip attributeSatisfies' isNothing
+
+lower :: Traversal' Element Node
+lower = nodes . traverse
+
+deepen :: Traversal' Element Element
+deepen = lower . _Element
+
+body :: Traversal' Document Element
+body = root . named "html" . deepen . named "body"
+
+div :: Traversal' Element Element
+div = named "div"
+
+h1 :: Traversal' Element Element
+h1 = named "h1"
+
+a :: Traversal' Element Element
+a = named "a"
+
+table :: Traversal' Element Element
+table = named "table"
+
+tr :: Traversal' Element Element
+tr = named "tr"
+
+td :: Traversal' Element Element
+td = named "td"
+
+cl :: Text -> Traversal' Element Element
+cl = attributeIs "class"
+
+id :: Text -> Traversal' Element Element
+id = attributeIs "id"

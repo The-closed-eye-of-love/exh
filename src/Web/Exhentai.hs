a9c63889e638415584bc74f85d1f6fe850fa1517
Poscat <poscat@mail.poscat.moe>
2021-01-28 22:02:38 +0800

    Export effects and effect handlers

--- src/Web/Exhentai.hs
+++ src/Web/Exhentai.hs
@@ -1,5 +1,35 @@
 module Web.Exhentai
-  ( -- ** Authentication
+  ( -- * Effects and effect interpreters
+
+    -- ** Important
+    Http,
+    formRequest,
+    genBoundary,
+    respOpen,
+    respClose,
+    ConduitIO (..),
+    runConduitIO,
+    Cookie (..),
+    takeCookie,
+    readCookie,
+    putCookie,
+    ExhC,
+    exhToIO,
+
+    -- ** Unimportant
+    HttpH,
+    HttpToIOC,
+    httpToIO,
+    CookieH,
+    CookieToIOC,
+    cookieToIO,
+    ConduitIOH,
+    ConduitIOToIOC,
+    conduitIOToIO,
+
+    -- * API
+
+    -- ** Authentication
     module Au,
 
     -- ** Getting information about galleries
@@ -16,12 +46,17 @@ module Web.Exhentai
 
     -- ** Fetching archives
     module Ar,
+
+    -- * Errors
+    ExhentaiError (..),
   )
 where
 
+import Control.Effect.Exh
 import Web.Exhentai.API.Archiver as Ar
 import Web.Exhentai.API.Auth as Au
 import Web.Exhentai.API.Gallery as G
 import Web.Exhentai.API.MPV as M
 import Web.Exhentai.API.Search as S
 import Web.Exhentai.API.Watched as W
+import Web.Exhentai.Errors
